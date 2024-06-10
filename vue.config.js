9857ec29e7619dc873452e3dae0b03991cc25742
Runas24 <runnnasss4@gmail.com>
2024-06-10 17:45:56 +0500

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/ri/'  // Замените 'ri' на имя вашего репозитория
+    : '/'
+};
